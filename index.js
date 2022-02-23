7f7b432381ccd760d89cf63878dc49206875d057
Josh Houghtelin <josh@findsomehelp.com>
2022-02-23 11:37:20 -0500

    init with person containing bad address response

---
+++ index.js
@@ -0,0 +1,70 @@
+var express = require('express');
+var app = express();
+
+app.get('/',(req, res) => {
+    res.send('<pre>Bender\n' +
+        '\n' +
+        '            o\n' +
+        '            |\n' +
+        '          ,\'~\'.\n' +
+        '         /     \\\n' +
+        '        |   ____|_\n' +
+        '        |  \'___,,_\'         .----------------.\n' +
+        '        |  ||(o |o)|       ( KILL ALL HUMANS! )\n' +
+        '        |   -------         ,----------------\'\n' +
+        '        |  _____|         -\'\n' +
+        '        \\  \'####,\n' +
+        '         -------\n' +
+        '       /________\\\n' +
+        '     (  )        |)\n' +
+        '     \'_ \' ,------|\\         _\n' +
+        '    /_ /  |      |_\\        ||\n' +
+        '   /_ /|  |     o| _\\      _||\n' +
+        '  /_ / |  |      |\\ _\\____//\' |\n' +
+        ' (  (  |  |      | (_,_,_,____/\n' +
+        '  \\ _\\ |   ------|\n' +
+        '   \\ _\\|_________|\n' +
+        '    \\ _\\ \\__\\\\__\\\n' +
+        '    |__| |__||__|\n' +
+        ' ||/__/  |__||__|\n' +
+        '         |__||__|\n' +
+        '         |__||__|\n' +
+        '         /__)/__)\n' +
+        '        /__//__/\n' +
+        '       /__//__/\n' +
+        '      /__//__/.\n' +
+        '    .\'    \'.   \'.\n' +
+        '   (_kOs____)____)\n' +
+        '\n' +
+        '------------------------------------------------\n' +
+        'This ASCII pic can be found at\n' +
+        'https://asciiart.website/index.php?art=television/futurama\n');
+});
+
+app.get('/person/:personId', (req, res) => {
+    const person =     {
+        email_address: "jhoughtelin+bender@alldigitalrewards.com",
+        unique_id: req.params.personId,
+        credit: 999999,
+        firstname: "Bender",
+        lastname: "Rodriguez",
+        language: "en_US",
+        address: {
+            firstname: "Bender",
+            lastname: "Rodriguez",
+            address1: "1337 Haxor Drive",  // Incorrect address for testing AVS
+            address2: "",
+            city: "Washington",
+            state: "DC",
+            zip: "20500",
+            country: "840",
+            country_code: "US"
+        },
+    };
+
+    res.json(person)
+});
+
+app.listen(80, () => {
+    console.log("My Story Is A Lot Like Yours, Only More Interesting ‘Cause It Involves Robots.");
+});
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Just another pointless mock API
+
+`docker-compose up` can run it locally, but it's not very useful.
+
+`node index.js` serves up the content but it doesn't do anything useful.
+
+We use this to construct tragically simple API responses to help QA
+test services that require remote APIs respond in specific ways.
