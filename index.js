f35c89753ce0d2a62675e8fe0a3cbfdb209e9fa2
Josh Houghtelin <josh@findsomehelp.com>
2023-08-15 08:12:14 -0700

    bork bork beep boop

--- index.js
+++ index.js
@@ -49,12 +49,12 @@ app.get('/person/:programId/:personId', (req, res) => {
         unique_id: req.params.personId,
         program: req.params.programId,
         credit: req.query.credit ?? 999999,
-        firstname: req.query.firstname ?? "Bender",
-        lastname: req.query.lastname ?? "R.",
+        firstname: req.query.firstname ?? "B.",
+        lastname: req.query.lastname ?? "Rodriguez",
         language: req.query.language ?? "en_US",
         address: {
-            firstname: req.query.firstname ?? "Bender",
-            lastname: req.query.lastname ?? "R.",
+            firstname: req.query.firstname ?? "B.",
+            lastname: req.query.lastname ?? "Rodriguez",
             address1: req.query.address1 ?? "1337 Haxor Drive",  // Incorrect address for testing AVS
             address2: req.query.address2 ?? "",
             city: req.query.city ?? "Washington",
